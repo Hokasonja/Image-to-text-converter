e89c7d18636cdd0e4f49741ddb8f493fc17f05a0
lee-3-8 <leesinji8@gmail.com>
2023-08-20 22:33:00 +0900

    test - output 테스트 코드 수정, 필요없는 테스트 코드 삭제

--- main.go
+++ main.go
@@ -1,24 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Hokasonja/Image-to-text-converter/src/converter"
 	"github.com/Hokasonja/Image-to-text-converter/src/file"
 )
 
-func hello() string {
-	return "Hello HokaSonja"
-}
-
-func plus(a, b int) int {
-	return a + b
-}
-
 func main() {
-
-	fmt.Println(hello())
-	fmt.Println(plus(2, 3))
 
 	prefix := "public/images/"
 
